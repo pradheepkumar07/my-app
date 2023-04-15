cec901917bad95792eb8cb3b8c873876d917f8a7
ses <ses@integra.co.in>
2023-04-15 13:31:26 +0530

    updated on 15th april

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,47 +1,53 @@
 const fs = require('fs');
 const http = require('http');
-
+const EventEmitter = require('events');
 const server = http.createServer();
+var express = require('express');
+var app = express();
 
-const read = fs.ReadStream('node.json', 'utf8');
-const write = fs.WriteStream('node.json', {flags: 'a'});
-/* server.on('request', (request, response) => {
-    if (request.url === '/data' request.method === 'GET') {
-        response.setHeader('Content-Type', 'application/json');
-        read.on('data', (data) => {
-            response.write(data)
-            response.end()
-        })
-    }else{
-        response.end()
-    }
-})
- */
+//Reading and write sterams for file system
+const read = fs.createReadStream('./nodejs/node.json', 'utf8');
+
+const write = fs.createWriteStream('./nodejs/node.json', {flags: 'a'});
+
+/* const handleRequests = new EventEmitter(); */
 
 server.on('request', (request, response) => {
     console.log("Testing...");
     if (request.url === "/articles" && request.method === "GET") {
       /* console.log(request.url); */
       response.setHeader('Content-Type', 'application/json');
-      read.on('articles', (data) => {
+      read.on('data', (data) => {
         response.write(data)
-        console.log(data);
+        // console.log(data);
         console.log("Testing ends...");
         response.end()
       })
-    } else {
+    } else{
+      response.end()
+    }
+    if (request.url === "/articles" && request.method === "POST"){
+      write.write(JSON.stringify({
+        articles1:[
+            {"id":8,"name":"user4","username":"user4","email":"user4@user4.com","password":"012345"},
+            {"id":9,"name":"user5","username":"user5","email":"user5@user5.com","password":"01234567"},
+            {"id":10,"name":"user5","username":"user5","email":"user5@user5.com","password":"0123456789"}
+          ]
+      }))
       response.end()
     }
   })
   
+var articles = JSON.parse(fs.readFileSync('./nodejs/node.json', 'UTF-8'));
+
+app.get('/articles/:id', function(req, res) {
+  var id = +req.params.id;
+  var article = articles.find(a => a.id === id);
+  res.write(article);
+  /* console.log(article); */
+})
 
 server.listen({port:3000},() => {
     console.log('server is running on port 3000');
-    write.write(JSON.stringify({
-      articles:[
-          {"id":4,"name":"user4","username":"user4","email":"user4@user4.com","password":"01234"},
-          {"id":5,"name":"user5","username":"user5","email":"user5@user5.com","password":"012345"}
-        ]
-    }))
 })
 
