eb49622153347e1add9bcac73475f68a3d9c1f5d
ses <ses@integra.co.in>
2023-04-26 18:57:33 +0530

    updates express

--- express/index.js
+++ express/index.js
@@ -2,6 +2,7 @@ const { id } = require('date-fns/locale');
 const data = require('./data.json');
 const express = require('express');
 const app = express();
+const fs = require('')
 
 app.listen(4000, err => {
     if(err) console.log(err)
@@ -24,4 +25,23 @@ app.get('/messages/:id', (req,res) => {
     res.send(temp)
     res.end()
 
+})
+
+app.get('/messages/:id/comments' ,(req,res) => {
+    random = req.params.id
+    const temp =  data.messages.filter( p => p.id === +random);
+    // console.log(JSON.stringify(temp));
+    let arrValues = Object.values(temp);
+    var comment = arrValues[0].content;
+    res.send(comment)
+    res.end()
+})
+
+app.post('/messages/:id/comments' ,(req,res) => {
+    random = req.params.id
+    // const temp =  data.messages.filter( p => p.id === +random);
+    // let arrValues = Object.values(temp);
+    // var comment = arrValues[0].content;
+    // console.log(comment)
+    res.end()
 })
