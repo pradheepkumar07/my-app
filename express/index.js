99c490087f6f808cdad030b92416d392e5e7bf24
ses <ses@integra.co.in>
2023-04-27 19:11:49 +0530

    updates

--- express/index.js
+++ express/index.js
@@ -2,7 +2,15 @@ const { id } = require('date-fns/locale');
 const data = require('./data.json');
 const express = require('express');
 const app = express();
-const fs = require('')
+const fs = require("fs");
+var bodyParser = require('body-parser');
+const { Delete } = require('@mui/icons-material');
+
+app.use(bodyParser.json());     //req.body
+
+const dbWrite = fs.WriteStream('./express/data2.json', {
+    flags: 'a'
+});
 
 app.listen(4000, err => {
     if(err) console.log(err)
@@ -39,9 +47,24 @@ app.get('/messages/:id/comments' ,(req,res) => {
 
 app.post('/messages/:id/comments' ,(req,res) => {
     random = req.params.id
-    // const temp =  data.messages.filter( p => p.id === +random);
-    // let arrValues = Object.values(temp);
-    // var comment = arrValues[0].content;
-    // console.log(comment)
+    const temp =  data.messages.filter( p => p.id === +random);
+    if(temp){  
+        temp[0].content = req.body.content
+        dbWrite.write(JSON.stringify(temp), {flags:"a"})
+        console.log("written");
+    }
+
+    console.log(temp)
+    let arrValues = Object.values(temp);
+    console.log(arrValues)
     res.end()
+})
+
+app.delete('/messages/:id/comments_id' ,(req,res) => {
+    random = req.params.id
+    const temp =  data.messages.filter( p => p.id === +random);
+    Delete(temp)
+    console.log(temp)
+    res.end()
+ 
 })
